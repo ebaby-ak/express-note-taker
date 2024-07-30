565fa24a708f024785b5b8ab9959ebf1eb318d8f
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-07-29 22:24:45 -0500

    added get path for index.html in index.js

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const html = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+
+app.listen(port, () => console.log('App listening on http://localhost:${PORT}')
+);
