9802334297f7ffc118faf3236d47b93324b845f1
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-07-29 22:08:15 -0500

    added the route js files and server.js file, updated server.js to link to route files and define port

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const api = require('./routes/api');
+const html = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
