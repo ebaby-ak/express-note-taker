f6b1a7412bd0549299c79a6916adfaa5aad24017
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-07-30 22:37:02 -0500

    added get and post to api.js

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,26 @@ const api = require('express').Router();
 const fs = require('fs');
 const uuid = require('uuid');
 
-api.get('./
+api.get('/', (req, res) => {
+    fs.readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+api.post('/', (req, res) => {
+    console.log(req.body);
+
+    const  {title, text, id} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            id: uuid(),
+            title,
+            text,
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+module.exports = api;
