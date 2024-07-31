1f1be809e72f31f4dd529257af2158d56fcb0802
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-07-30 22:13:44 -0500

    added middleware to server.js

---
+++ routes/api.js
@@ -0,0 +1,5 @@
+const api = require('express').Router();
+const fs = require('fs');
+const uuid = require('uuid');
+
+api.get('./
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/routes/index.js', html);
+app.use('/api', api);
 
 app.listen(port, () => console.log('App listening on http://localhost:${PORT}')
 );
