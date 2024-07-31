0edb9029c0af2abe517599123b99202d8e501fee
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-07-30 23:05:12 -0500

    working through bugs with GET and POST

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,20 @@
 const api = require('express').Router();
 const fs = require('fs');
 const uuid = require('uuid');
-
-api.get('/', (req, res) => {
+api.get('/api/notes', (req, res) => {
     fs.readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-api.post('/', (req, res) => {
+api.post('/api/notes', (req, res) => {
     console.log(req.body);
 
-    const  {title, text, id} = req.body;
+    const {title, text, id} = req.body;
 
     if (req.body) {
         const newNote = {
             id: uuid(),
-            title,
-            text,
+            title: req.body.title,
+            text: req.body.text,
         };
         readAndAppend(newNote, './db/db.json');
         res.json(`Note added successfully`);
