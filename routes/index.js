50b0b93e4ed3097a9b1535e2e715b6577fbe524f
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-09-18 15:55:21 -0500

    added post route that will handle new notes and return status

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 const router = require('express').Router();
 const path = require('path');
+const { UUIDV4 } = require('sequelize');
 
-router.get('/api/notes', (req, res) => {
+router.get('/api/notes', async (req, res) => {
 const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
 res.json(dbJson);
 });
 
-router.get('/notes', (req, res) => {
-res.sendFile(path.join(__dirname, '../public/assets/notes.html'))
+router.post('/api/notes', (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    dbJson.push(newNote);
+    fs.writeFileSync("db/db.json", JSON.stringify(dbJson));
+    res.json(dbJson);
+
+    return res.status(201).json({
+        status: "successful",
+        data: newNote,
+    });
 });
 
 module.exports = router;
