8c6a536f0fe06d136c1cac027acab26707055cd6
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-09-29 20:54:06 -0500

    further resolved routes in index.js

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(html);
+
 app.use(api);
+
+app.use(express.static('public'));
+
+app.use(html);
+
 
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`)
 );
