96ffdccf562e1f9628a048d240a5ac6345be9ddc
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-09-29 20:48:19 -0500

    updated issues with routes

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,24 @@
 const router = require('express').Router();
 const fs = require('fs');
-const uuidv4 = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
+const dbPath = path.join(__dirname, '../db/db.json');
 
 router.get("/api/notes", async (req, res) => {
-  const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  const dbJson = await JSON.parse(fs.readFileSync(dbPath, "utf8"));
   res.json(dbJson);
 });
 
 router.post("/api/notes", (req, res) => {
-  const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  const dbJson = JSON.parse(fs.readFileSync(dbPath, "utf8"));
   const newNote = {
     title: req.body.title,
     text: req.body.text,
     id: uuidv4(),
   };
   dbJson.push(newNote);
-  fs.writeFileSync("db/db.json", JSON.stringify(dbJson));
-  res.json(dbJson);
+  fs.writeFileSync(dbPath, JSON.stringify(dbJson));
 
   return res.status(201).json({
     status: "successful",
@@ -25,12 +27,12 @@ router.post("/api/notes", (req, res) => {
 });
 
 router.delete("/api/notes/:id", (req, res) => {
-  let data = fs.readFileSync("db/db.json", "utf8");
+  let data = fs.readFileSync(dbPath, "utf8");
   const dataJson = JSON.parse(data);
-  const noteIndex = dataJson.findIndex((note) => {
-    return note.id === req.params.id;
-  });
-  fs.writeFileSync("db/db.json", JSON.stringify(noteIndex));
+  const noteIndex = dataJson.findIndex((note) => 
+    note.id === req.params.id);
+
+  fs.writeFileSync(dbPath, JSON.stringify(noteIndex));
   res.json("Note deleted successfully");
 });
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(html);
 app.use(api);
